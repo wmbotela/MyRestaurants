0e1ec5c250df9e552ade1c8e7cb460b83344eac3
wmbotela <wmbotela@gmail.com>
2017-05-30 09:44:09 +0300

    My Restaurant app with Text view and toast

---
+++ app/src/androidTest/java/com/moringaschool/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -0,0 +1,40 @@
+package com.moringaschool.myrestaurants;
+
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.core.IsNot.not;
+
+/**
+ * Created by william on 5/24/17.
+ */
+
+public class RestaurantsActivityInstrumentationTest {
+    @Rule
+    public ActivityTestRule<RestaurantsActivity> activityTestRule =
+            new ActivityTestRule<>(RestaurantsActivity.class);
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant() {
+        View activityDecorView = activityTestRule.getActivity().getWindow().getDecorView();
+        String restaurantName = "Carnivore";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
+
+}
---
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -0,0 +1,41 @@
+package com.moringaschool.myrestaurants;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @Bind(R.id.locationEditText) EditText mLocationEditText;
+    @Bind(R.id.appNameTextView) TextView mAppNameTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/ostrichregular.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
+
+        mFindRestaurantsButton.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        if(v == mFindRestaurantsButton) {
+            String location = mLocationEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+            intent.putExtra("location", location);
+            startActivity(intent);
+        }
+    }
+}
